abed0515a960749dc8b4b8306824bbb28ae8c933
Marek Ryba <ryba231@gmail.com>
2026-01-24 19:43:19 +0100

    fixed insert

--- migrations/Version20260119182359.php
+++ migrations/Version20260119182359.php
@@ -21,7 +21,7 @@ final class Version20260119182359 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE user ADD anonymized_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', ADD is_active TINYINT(1) NOT NULL');
-        $this->addSql('INSERT INTO user (id, email, first_name, last_name, password,is_active) VALUES (1, \'system@internal\', \'system\', \'system\', \'$2y$13$zKLYF85ToqWY1kM3wetJouvEU0zaPsYRbybQQzdJTzt\',0)');
+        $this->addSql('INSERT INTO `user` (`id`, `email`, `first_name`, `last_name`, `roles`, `password`, `created_at`, `deleted_at`, `anonymized_at`, `is_active`) VALUES (1, \'system@internal\', \'system\', \'system\', NULL, \'$2y$13$zKLYF85ToqWY1kM3wetJouvEU0zaPsYRbybQQzdJTzt\', \'2026-01-06 14:10:54\', NULL, NULL, 0);');
     }
 
     public function down(Schema $schema): void
